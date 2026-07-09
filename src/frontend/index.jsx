e4ee4aa1aa31bb507206af56567e450c5ed1dd7e
Aman <amanlokhande7@gmail.com>
2026-07-09 13:34:17 -0500

    Update index.jsx

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
-import { invoke } from '@forge/bridge';
+import React, { useEffect, useState } from "react"
+import ForgeReconciler, { Text } from "@forge/react"
+import { invoke } from "@forge/bridge"
 
 const App = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null)
 
   useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+    invoke("getText", { example: "my-invoke-variable" }).then(setData)
+  }, [])
 
   return (
     <>
       <Text>Hello world! yo</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
+      <Text>This is test text. Is it working for you guys?</Text>
+      <Text>{data ? data : "Loading..."}</Text>
     </>
-  );
-};
+  )
+}
 
 ForgeReconciler.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
